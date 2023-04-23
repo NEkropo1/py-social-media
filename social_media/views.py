d33e6abbfadfe42217f599bfa30dee47c1a2d6b1
Paul Savchuk <nekrjkeee@gmail.com>
2023-04-24 02:00:25 +0300

    add filtering for posts

--- social_media/views.py
+++ social_media/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Q
 from rest_framework import generics
 
 from social_media.models import Post
 from .serializers import (
-    PostListSerializer, PostDetailSerializer
+    PostListSerializer,
+    PostDetailSerializer
 )
 
 
@@ -12,6 +14,25 @@ class PostListView(generics.CreateAPIView, generics.ListAPIView):
     """
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
+
+    def get_queryset(self):
+        followed_users_ids = (
+            self.request.user.followers.all()
+        )
+        user_profile_id = self.request.user.id
+
+        queryset = self.queryset.filter(
+            owner__in=list(followed_users_ids) + [user_profile_id]
+        )
+
+        hashtags = self.request.query_params.get("hashtags")
+        if hashtags:
+            query = Q()
+            for hashtag in hashtags.split(','):
+                query |= Q(message__icontains=f"#{hashtag}")
+            queryset = queryset.filter(query)
+
+        return queryset.distinct()
 
 
 class PostDetailView(generics.RetrieveUpdateAPIView):
