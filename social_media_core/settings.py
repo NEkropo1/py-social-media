c275e5f40f4536740af62debbfa86ca68f35e2c9
Paul Savchuk <nekrjkeee@gmail.com>
2023-05-04 23:54:08 +0300

    refactor readme and add env.sample for appropriate work

--- social_media_core/settings.py
+++ social_media_core/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-luq3)v&4yq3ejp-4g#%zob5l5t^_o0w4k3+obi8sbv!&k50b(x"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
